018406b130297df105fc12c23316882049ef54d2
nero-15 <nero15.dev@gmail.com>
2021-07-25 20:43:33 +0900

    fix

--- graphql-learning-app/src/index.js
+++ graphql-learning-app/src/index.js
@@ -1,31 +1,15 @@
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: async (parent, args, context) => {
-            return context.prisma.link.findMany()
-        },
-    },
-    Mutation: {
-        post: (parent, args, context, info) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                },
-            })
-            return newLink
-        },
-    },
+  Query,
+  Mutation,
+  User,
+  Link
 }
 
 const fs = require('fs');
